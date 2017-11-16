59b0ae4bdbfa1ad09c1eacadc60e1aecb140cb3e
Kasatkina Mariya <kasatkina.mariya@gmail.com>
2017-11-16 09:21:21 +0500

    no need to specify EntityEnumValues in InheritanceConfiguration

--- Model.Interfaces/EntityType.cs
+++ Model.Interfaces/EntityType.cs
@@ -1,10 +1,8 @@
-﻿using LinqToDB.Mapping;
-
-namespace Model.Interfaces
+﻿namespace Model.Interfaces
 {
     public enum EntityType
     {
-        [MapValue(1)] TypeA,
-        [MapValue(2)] TypeB,
+        TypeA,
+        TypeB,
     }
 }
